0e1624be2a993b7c6de2189a9c0de19fc136dfae
poornimayo <55318034+poornimayo@users.noreply.github.com>
2021-07-20 14:32:24 +0530

    Update jenkinsForJava.groovy

--- vars/jenkinsForJava.groovy
+++ vars/jenkinsForJava.groovy
@@ -1,38 +1,45 @@
 def call(String repoUrl) {
   pipeline {
        agent any
-       tools {
-           maven 'Maven 3.5.0'
-           jdk 'jdk8'
-       }
+       environment{
+        PATH="C:/ProgramData/chocolatey/lib/maven/apache-maven-3.6.3/bin:$PATH"
+    }
        stages {
-           stage("Tools initialization") {
-               steps {
-                   sh "mvn --version"
-                   sh "java -version"
-               }
-           }
+           
            stage("Checkout Code") {
                steps {
                    git branch: 'master',
                        url: "${repoUrl}"
                }
            }
-           stage("Cleaning workspace") {
-               steps {
-                   sh "mvn clean"
-               }
-           }
-           stage("Running Testcase") {
-              steps {
-                   sh "mvn test"
-               }
-           }
-           stage("Packing Application") {
-               steps {
-                   sh "mvn package -DskipTests"
-               }
-           }
+           stage('build'){
+            steps{
+                bat '''mvn clean package
+'''
+            }
+        }
+        stage('package publisher'){
+            steps{
+                nexusPublisher nexusInstanceId: 'localnexus3', nexusRepositoryId: 'maven-releases', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: 'C:\\Windows\\System32\\config\\systemprofile\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\first-pipeline\\target\\hello-world-war-1.0.0.war']], mavenCoordinate: [artifactId: 'HelloWOrld', groupId: 'com.helloWOrld', packaging: 'war', version: '1.7']]]
+            }
+        }
        }
+    post {
+failure {
+  script {
+    if (currentBuild.currentResult == 'FAILURE') {
+      step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: "poornimayo98@gmail.com", sendToIndividuals: true])
+    }
+  }
+}
+success{
+    script{
+    if (currentBuild.currentResult == 'SUCCESS') {
+      step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: "poornimayo98@gmail.com", sendToIndividuals: true])
+    }
+    }
+}
+  
+}
    }
 }

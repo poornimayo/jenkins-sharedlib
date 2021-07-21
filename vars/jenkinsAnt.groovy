c9f1f118ed9ef78a6b3ade34d3a88f0cacb12127
poornimayo <55318034+poornimayo@users.noreply.github.com>
2021-07-21 16:17:36 +0530

    Update and rename jenkinsLambdaTest.groovy to jenkinsAnt.groovy

---
+++ vars/jenkinsAnt.groovy
@@ -0,0 +1,44 @@
+def call(String repoUrl) {
+  pipeline {
+       agent any
+       environment{
+        PATH="C:/ProgramData/chocolatey/lib/ant/tools/apache-ant-1.10.11/bin:$PATH"
+    }
+       stages {
+           
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'master',
+                       url: "${repoUrl}"
+               }
+           }
+           stage('build'){
+            steps{
+                bat 'ant clean compile test package war'
+            }
+        }
+        stage('deploy'){
+            steps{
+                nexusPublisher nexusInstanceId: 'localnexus3', nexusRepositoryId: 'maven-releases', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: 'C:\\Windows\\System32\\config\\systemprofile\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\first-project-ant\\target\\roshambo.war']], mavenCoordinate: [artifactId: 'antProject', groupId: 'com.antproject', packaging: 'war', version: '1.2']]]
+            }
+        }
+       }
+    post {
+failure {
+  script {
+    if (currentBuild.currentResult == 'FAILURE') {
+      step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: "poornimayo98@gmail.com", sendToIndividuals: true])
+    }
+  }
+}
+success{
+    script{
+    if (currentBuild.currentResult == 'SUCCESS') {
+      step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: "poornimayo98@gmail.com", sendToIndividuals: true])
+    }
+    }
+}
+  
+}
+   }
+}
